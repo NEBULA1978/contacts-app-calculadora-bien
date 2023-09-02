2dcc8c18ef87c7ee32000f49ea4ea3370440ea48
NEBULA1978 <rpascualey@gmail.com>
2023-09-02 02:09:30 +0200

    Añado calculadora php imc a crud sarosi

---
+++ delete.php
@@ -0,0 +1,36 @@
+<?php
+
+require "database.php";
+
+session_start();
+
+if (!isset($_SESSION["user"])) {
+  header("Location: login.php");
+  return;
+}
+
+$id = $_GET["id"];
+
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id LIMIT 1");
+$statement->execute([":id" => $id]);
+
+if ($statement->rowCount() == 0) {
+  http_response_code(404);
+  echo("HTTP 404 NOT FOUND");
+  return;
+}
+
+// Para que no pueda un ususario registrado borrar a otro
+$contact = $statement->fetch(PDO::FETCH_ASSOC);
+
+if ($contact["user_id"] !== $_SESSION["user"]["id"]) {
+  http_response_code(403);
+  echo("HTTP 403 UNAUTHORIZED");
+  return;
+}
+
+$conn->prepare("DELETE FROM contacts WHERE id = :id")->execute([":id" => $id]);
+
+$_SESSION["flash"] = ["message" => "Contact {$contact['name']} deleted."];
+
+header("Location: home.php");
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require "partials/header.php" ?>
+
+<div class="welcome d-flex align-items-center justify-content-center">
+  <div class="text-center">
+    <h1>Store Your Contacts Now</h1>
+    <a class="btn btn-lg btn-dark" href="register.php">Get Started</a>
+    <a class="btn btn-lg btn-primary" href="imc/index.php">Go to IMC</a> <!-- Agrega este botón -->
+    <img src="./imc/imagenes/salud.jpg" alt="">
+  </div>
+</div>
+
+<?php require "partials/footer.php" ?>
